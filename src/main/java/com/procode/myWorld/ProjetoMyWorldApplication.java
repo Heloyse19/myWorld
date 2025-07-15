ac766f513d3582714444cf4146a35c045b8169df
Heloyse19 <sraheloysesantos@gmail.com>
2025-07-15 16:23:50 -0300

    commit inicial

---
+++ src/main/java/com/procode/myWorld/ProjetoMyWorldApplication.java
@@ -0,0 +1,13 @@
+package com.procode.myWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMyWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMyWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/procode/myWorld/ProjetoMyWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procode.myWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMyWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
