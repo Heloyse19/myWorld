6e5ce6062bcc2dec7eafc12a21d931a904fba897
Heloyse19 <sraheloysesantos@gmail.com>
2025-07-17 17:27:58 -0300

    criando controller, modelo e repository

---
+++ src/main/java/com/procode/myWorld/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.procode.myWorld.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping("/api")
+    public String Home(){
+        return "oi";
+    }
+
+}
---
+++ src/main/java/com/procode/myWorld/models/Usuario.java
@@ -0,0 +1,24 @@
+package com.procode.myWorld.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private boolean ativo = true;
+}
